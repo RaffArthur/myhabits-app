1fedf44eeb03ef4ec12dfcae8b9e9b5b144bda35
Arthur Raff <arthur.raff@icloud.com>
2020-11-08 13:00:32 +0300

    Initial Commit

---
+++ MyHabits_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits_App
+//
+//  Created by Arthur Raff on 08.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
